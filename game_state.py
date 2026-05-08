3a9e4933f35dca66f7062c7a5a750a591f59674d
Corbin Heckler <corbiheckler10@gmail.com>
2026-05-08 13:35:15 -0500

    menus

---
+++ game_state.py
@@ -0,0 +1,6 @@
+
+HOME_MENU = "home menu"
+BATTLE = "battle"
+MAP = "map"
+GAME_OVER = "game_over"
+CHARACTER_SELECT = "character_select"
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import pygame
 from button import Button
+from game_state import HOME_MENU, BATTLE , CHARACTER_SELECT
+
 pygame.init()
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, DARK_BG
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Roguelike Game")
-BLACK= (0, 0, 0)
-WHITE = (255, 255, 255)
 font = pygame.font.Font(None, 50)
 start_button = Button(300, 170, 200, 60, "Start")
 quit_button = Button(300,250,200,60,"Quit")
-
+current_state = HOME_MENU
 
 
 
@@ -21,11 +21,11 @@ while running:
             if quit_button.is_clicked(event.pos):
                 running = False
             if start_button.is_clicked(event.pos):
-                running = False
+                current_state = CHARACTER_SELECT
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((DARK_BG))
+    screen.fill(DARK_BG)
     
     start_button.draw(screen, font)
     quit_button.draw(screen, font)
